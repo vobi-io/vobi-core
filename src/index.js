7ff984f408a96715eaf1c298fde97b711e575b3f
Giga Chkhikvadze <giga.chkhikvadze@gmail.com>
2019-10-01 12:45:21 +0400

    fix: export errors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 export { default as BaseModelPlugin } from './core/baseModelPlugin'
 export * from './core/graphql'
 export { default as MaskUSPhone } from './Phone'
-export {default as errors} from './utils/apiErrors'
+
+export {default as apiErrors} from './utils/apiErrors/apiErrors'
+export {default as onErrorExpress} from './utils/apiErrors/onErrorExpress'
+export {default as onErrorGraphql} from './utils/apiErrors/onErrorGraphql'
+export {default as responseDecorator} from './utils/apiErrors/responseDecorator'
