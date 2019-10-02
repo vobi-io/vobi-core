55206b1970d2372b68c5b186baa2aa3ebdd17ccb
Giga Chkhikvadze <giga.chkhikvadze@gmail.com>
2019-10-02 12:09:34 +0400

    fix: update packages

--- src/index.js
+++ src/index.js
@@ -6,4 +6,6 @@ export {default as apiErrors} from './utils/apiErrors/apiErrors'
 export {default as onErrorExpress} from './utils/apiErrors/onErrorExpress'
 export {default as onErrorGraphql} from './utils/apiErrors/onErrorGraphql'
 export {default as responseDecorator} from './utils/apiErrors/responseDecorator'
+export {default as utils} from './utils'
 export { modelsFromModules, collectModulePaths, getModuleName } from './modules'
+
